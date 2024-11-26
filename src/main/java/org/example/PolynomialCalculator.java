b5e8b176d3098522603f34eab3ea3d2996eb940d
yunho <nam100400@naver.com>
2024-11-26 12:21:25 +0900

    Implementing the method to match the testCode

--- src/main/java/org/example/PolynomialCalculator.java
+++ src/main/java/org/example/PolynomialCalculator.java
@@ -3,6 +3,11 @@ package org.example;
 public class PolynomialCalculator {
 
     public static int run(String expr){
-        return 0;
+        String[] exprArr = expr.split(" \\+ ");
+
+        int num1 = Integer.parseInt(exprArr[0]);
+        int num2 = Integer.parseInt(exprArr[1]);
+
+        return num1 + num2;
     }
 }
