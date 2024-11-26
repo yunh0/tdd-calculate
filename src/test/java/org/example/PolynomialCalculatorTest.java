87cd795efadd59e90e71bf6c4e564d3cc5d25ef0
yunho <nam100400@naver.com>
2024-11-26 12:25:03 +0900

    Create a polynomial test case

--- src/test/java/org/example/PolynomialCalculatorTest.java
+++ src/test/java/org/example/PolynomialCalculatorTest.java
@@ -9,9 +9,17 @@ class PolynomialCalculatorTest {
 
     @Test
     @DisplayName("3 + 5")
-    void test1(){
+    void simpleTest(){
         int rs = PolynomialCalculator.run("3 + 5");
 
         assertThat(rs).isEqualTo(8);
+    }
+
+    @Test
+    @DisplayName("((3 + 5) * 5 + -10) * 10 / 5 = 60")
+    void polynomialCalculationTest(){
+        int rs = PolynomialCalculator.run("((3 + 5) * 5 + -10) * 10 / 5");
+
+        assertThat(rs).isEqualTo(60);
     }
 }
