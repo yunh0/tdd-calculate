f503fece70365e95236504565960da5621ab7ea1
yunho <nam100400@naver.com>
2024-11-26 11:13:29 +0900

    create add method

---
+++ src/main/java/org/example/SimpleCalculator.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class SimpleCalculator {
+
+    public int plus(int num1, int num2){
+        return num1 + num2;
+    }
+}
--- src/test/java/org/example/SimpleCalculatorTest.java
+++ src/test/java/org/example/SimpleCalculatorTest.java
@@ -3,18 +3,27 @@ package org.example;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SimpleCalculatorTest {
 
     @Test
     @DisplayName("덧셈 : 10 + 20 = 30")
-    public void t1(){
+    public void addTest1(){
         SimpleCalculator calculator = new SimpleCalculator();
 
         int rs = calculator.plus(10, 20);
 
         assertEquals(rs, 30);
+    }
+
+    @Test
+    @DisplayName("덧셈 : 1000 + 9000 = 10000")
+    public void addTest2(){
+        SimpleCalculator calculator = new SimpleCalculator();
+
+        int rs = calculator.plus(1000, 9000);
+
+        assertEquals(rs, 10000);
     }
 }
